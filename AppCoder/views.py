d7f4829119c35f2c65c707da2bdea02918ac8a9d
Franco Romero <fgromero2003@gmail.com>
2023-03-06 17:08:12 -0300

    proyecto clas19 corregido

--- AppCoder/views.py
+++ AppCoder/views.py
@@ -2,11 +2,18 @@ from django.shortcuts import render
 from AppCoder.models import Curso
 from django.http import HttpResponse
 # Create your views here.
-def curso(self):
 
-    curso = Curso(nombre = 'Desarrollo Web', camada = 12345)
-    curso.save()
+def inicio(request):
+    return render(request, 'inicio.html')
 
-    documentoDeTexto = f'---> Curso: {curso.nombre} Camada: {curso.camada}'
+def cursos(request):
+    return render(request, 'cursos.html')
 
-    return HttpResponse(documentoDeTexto)
+def profesores(request):
+    return render(request, 'profesores.html')
+
+def estudiantes(request):
+    return render(request, 'estudiantes.html')
+
+def entregables(request):
+    return render(request, 'entregables.html')
